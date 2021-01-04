9fa2575ed81399b758e7023634b744e6f986221e
jfcorugedo <jfcorugedo@gmail.com>
2021-01-04 16:41:26 +0100

    feat(Singleton): adds CalculatorSingletonTest.somSeveralNumbers test

--- src/test/java/com/jfcorugedo/creational/singleton/CalculatorSingletonTest.java
+++ src/test/java/com/jfcorugedo/creational/singleton/CalculatorSingletonTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * TDD Tests lists:
+ *
+ * - Sum just one number should return the same number
+ * - Sum two or more numbers should return the sum of all the values
+ * - Since the implementation is a Singleton, there should be just one instance of the Calculator
+ *
+ */
 public class CalculatorSingletonTest {
 
     @Test
@@ -12,5 +20,13 @@ public class CalculatorSingletonTest {
         CalculatorSingleton calculator = CalculatorSingleton.getInstance();
 
         assertThat(calculator.sum(1)).isEqualTo(1);
+    }
+
+    @Test
+    public void sumSeveralNumbers() {
+
+        CalculatorSingleton calculator = CalculatorSingleton.getInstance();
+
+        assertThat(calculator.sum(0,1)).isEqualTo(1);
     }
 }
