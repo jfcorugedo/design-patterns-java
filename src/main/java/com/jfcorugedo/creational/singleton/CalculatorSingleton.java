4c32d8dbc421abac9a5b6230f0cf30c99fce87e6
jfcorugedo <jfcorugedo@gmail.com>
2021-01-04 16:35:32 +0100

    feat(Singleton): make CalculatorSingletonTest.sumOneNumber compile

---
+++ src/main/java/com/jfcorugedo/creational/singleton/CalculatorSingleton.java
@@ -0,0 +1,12 @@
+package com.jfcorugedo.creational.singleton;
+
+public class CalculatorSingleton {
+
+    public static CalculatorSingleton getInstance() {
+        return null;
+    }
+
+    public int sum(int numbers) {
+        return 0;
+    }
+}
