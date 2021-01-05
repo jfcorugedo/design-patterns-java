8d8b6e550adc85c7022a9c2ee3e14a41cca0d7ab
jfcorugedo <jfcorugedo@gmail.com>
2021-01-05 17:13:10 +0100

    feat(Prototype): makes RectangleTest.hasDefaultWidth compile and pass using Fake It approach

---
+++ src/main/java/com/jfcorugedo/creational/prototype/PrototypeManager.java
@@ -0,0 +1,8 @@
+package com.jfcorugedo.creational.prototype;
+
+public class PrototypeManager {
+
+    public static Rectangle buildRectangle() {
+        return new Rectangle();
+    }
+}
---
+++ src/main/java/com/jfcorugedo/creational/prototype/Rectangle.java
@@ -0,0 +1,7 @@
+package com.jfcorugedo.creational.prototype;
+
+public class Rectangle {
+    public int getWidth() {
+        return 20;
+    }
+}
