2908b457b3f0a039418d576feea734bf00928cb4
jfcorugedo <jfcorugedo@gmail.com>
2021-01-05 10:49:09 +0100

    chore(Builder): adds PersonTest.builderCanSetNameAttribute test

--- src/test/java/com/jfcorugedo/creational/builder/PersonTest.java
+++ src/test/java/com/jfcorugedo/creational/builder/PersonTest.java
@@ -4,6 +4,13 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * TDD tests list:
+ * - Builder can create instances of Person
+ * - Builder can set name attribute inside Person instances
+ * - Builder can set all the skills of this person
+ * - Person instances are immutable
+ */
 public class PersonTest {
 
     @Test
@@ -12,5 +19,13 @@ public class PersonTest {
         Person person = Person.builder().build();
 
         assertThat(person).isNotNull();
+    }
+
+    @Test
+    public void builderCanSetNameAttribute() {
+
+        Person person = Person.builder().name("Juan").build();
+
+        assertThat(person.getName()).isEqualTo("Juan");
     }
 }
