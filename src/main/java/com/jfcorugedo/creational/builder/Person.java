26a43f8b662b3e749752742e522585240d154998
jfcorugedo <jfcorugedo@gmail.com>
2021-01-05 11:16:47 +0100

    feat(Builder): refactor (eliminate duplicates) after PersonTest.builderCanSetSkills is green

--- src/main/java/com/jfcorugedo/creational/builder/Person.java
+++ src/main/java/com/jfcorugedo/creational/builder/Person.java
@@ -1,26 +1,29 @@
 package com.jfcorugedo.creational.builder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
 
     private String name;
+    private List<String> skills;
 
     public String getName() {
         return this.name;
     }
 
     public List<String> getSkills() {
-        return List.of("Java");
+        return this.skills;
     }
 
     public static class PersonBuilder {
         private String name;
+        private List<String> skills = new ArrayList<>();
 
         PersonBuilder(){}
 
         public Person build() {
-            return new Person(this.name);
+            return new Person(this.name, this.skills);
         }
 
         public PersonBuilder name(String name) {
@@ -29,11 +32,15 @@ public class Person {
         }
 
         public PersonBuilder skill(String skill) {
+            this.skills.add(skill);
             return this;
         }
     }
 
-    private Person(String name) {this.name = name;}
+    private Person(String name, List<String> skills) {
+        this.name = name;
+        this.skills = skills;
+    }
 
     public static PersonBuilder builder() {
         return new PersonBuilder();
