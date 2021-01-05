0cd8fa43442d334033bcb49cab256151d76afe3c
jfcorugedo <jfcorugedo@gmail.com>
2021-01-05 10:50:32 +0100

    chore(Builder): makes PersonTest.builderCanSetNameAttribute compile

--- src/main/java/com/jfcorugedo/creational/builder/Person.java
+++ src/main/java/com/jfcorugedo/creational/builder/Person.java
@@ -2,10 +2,18 @@ package com.jfcorugedo.creational.builder;
 
 public class Person {
 
+    public String getName() {
+        return null;
+    }
+
     public static class PersonBuilder {
         PersonBuilder(){}
         public Person build() {
             return new Person();
+        }
+
+        public PersonBuilder name(String name) {
+            return null;
         }
     }
 
