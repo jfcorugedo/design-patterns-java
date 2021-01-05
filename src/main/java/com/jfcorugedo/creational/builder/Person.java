e99b0110b960a37a524c7da30de25d9d45b82b50
jfcorugedo <jfcorugedo@gmail.com>
2021-01-05 10:53:21 +0100

    chore(Builder): refactor (eliminate duplicates) PersonTest.builderCanSetNameAttribute after it is in green

--- src/main/java/com/jfcorugedo/creational/builder/Person.java
+++ src/main/java/com/jfcorugedo/creational/builder/Person.java
@@ -2,22 +2,28 @@ package com.jfcorugedo.creational.builder;
 
 public class Person {
 
+    private String name;
+
     public String getName() {
-        return "Juan";
+        return this.name;
     }
 
     public static class PersonBuilder {
+        private String name;
+
         PersonBuilder(){}
+
         public Person build() {
-            return new Person();
+            return new Person(this.name);
         }
 
         public PersonBuilder name(String name) {
+            this.name = name;
             return this;
         }
     }
 
-    private Person() {}
+    private Person(String name) {this.name = name;}
 
     public static PersonBuilder builder() {
         return new PersonBuilder();
