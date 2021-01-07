ff231ac83d019ec5ed23ceddac2c66c6d617b4a7
jfcorugedo <jfcorugedo@gmail.com>
2021-01-08 00:24:45 +0100

    feat(FactoryMethod): refactor to use EnumMap instead of HashMap and method references instead of lambda expressions

--- src/main/java/com/jfcorugedo/creational/factorymethod/ButtonFactory.java
+++ src/main/java/com/jfcorugedo/creational/factorymethod/ButtonFactory.java
@@ -4,17 +4,16 @@ import com.jfcorugedo.creational.factorymethod.behaviours.Clickable;
 import com.jfcorugedo.creational.factorymethod.buttons.PopupButton;
 import com.jfcorugedo.creational.factorymethod.buttons.SubmitButton;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.EnumMap;
 import java.util.function.Supplier;
 
 public class ButtonFactory {
 
-    private Map<Clickable.Type, Supplier<Clickable>> buttons = new HashMap<>();
+    private EnumMap<Clickable.Type, Supplier<Clickable>> buttons = new EnumMap<>(Clickable.Type.class);
 
     public ButtonFactory() {
-        buttons.put(Clickable.Type.SUBMIT, () -> new SubmitButton());
-        buttons.put(Clickable.Type.POPUP, () -> new PopupButton());
+        buttons.put(Clickable.Type.SUBMIT, SubmitButton::new);
+        buttons.put(Clickable.Type.POPUP, PopupButton::new);
     }
 
     public Clickable createClickable(Clickable.Type type) {
