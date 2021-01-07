3ca16ee05eac205babd9ff02d4cf352f0003bc30
jfcorugedo <jfcorugedo@gmail.com>
2021-01-08 00:51:05 +0100

    feat(FactoryMethod): refactor (eliminate duplicates) of InputFactoryTest.inputTextIsClickable. Now new elements can by added just putting a new Supplier in the EnumMap

--- src/main/java/com/jfcorugedo/creational/factorymethod/InputFactory.java
+++ src/main/java/com/jfcorugedo/creational/factorymethod/InputFactory.java
@@ -3,9 +3,19 @@ package com.jfcorugedo.creational.factorymethod;
 import com.jfcorugedo.creational.factorymethod.behaviours.Clickable;
 import com.jfcorugedo.creational.factorymethod.inputs.InputText;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class InputFactory {
 
+    private Map<Clickable.Type, Supplier<Clickable>> inputs = new EnumMap<>(Clickable.Type.class);
+
+    public InputFactory() {
+        inputs.put(Clickable.Type.INPUT_TEXT, InputText::new);
+    }
+
     public Clickable createClickable(Clickable.Type type) {
-        return new InputText();
+        return inputs.get(type).get();
     }
 }
