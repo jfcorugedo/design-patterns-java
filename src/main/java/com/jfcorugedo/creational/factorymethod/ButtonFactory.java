b60dda05a930e11af894258b68ae334088a74cdd
jfcorugedo <jfcorugedo@gmail.com>
2021-01-08 00:22:19 +0100

    feat(FactoryMethod): refactor (eliminate duplicates) to remove switch statement and use a set of Suppliers to build buttons

--- src/main/java/com/jfcorugedo/creational/factorymethod/ButtonFactory.java
+++ src/main/java/com/jfcorugedo/creational/factorymethod/ButtonFactory.java
@@ -4,13 +4,21 @@ import com.jfcorugedo.creational.factorymethod.behaviours.Clickable;
 import com.jfcorugedo.creational.factorymethod.buttons.PopupButton;
 import com.jfcorugedo.creational.factorymethod.buttons.SubmitButton;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class ButtonFactory {
 
+    private Map<Clickable.Type, Supplier<Clickable>> buttons = new HashMap<>();
+
+    public ButtonFactory() {
+        buttons.put(Clickable.Type.SUBMIT, () -> new SubmitButton());
+        buttons.put(Clickable.Type.POPUP, () -> new PopupButton());
+    }
+
     public Clickable createClickable(Clickable.Type type) {
-        switch (type) {
-            case SUBMIT: return new SubmitButton();
-            case POPUP: return new PopupButton();
-            default: return () -> null;
-        }
+
+        return buttons.get(type).get();
     }
 }
