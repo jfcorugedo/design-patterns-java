3e82f5d3846c495fba494a48721df64c1c4b6949
jfcorugedo <jfcorugedo@gmail.com>
2021-01-08 01:01:46 +0100

    feat(FactoryMethod): makes InputFactoryTest.inputTextProducesWritableActionWhenClicked pass

--- src/main/java/com/jfcorugedo/creational/factorymethod/inputs/InputText.java
+++ src/main/java/com/jfcorugedo/creational/factorymethod/inputs/InputText.java
@@ -1,11 +1,12 @@
 package com.jfcorugedo.creational.factorymethod.inputs;
 
 import com.jfcorugedo.creational.factorymethod.actions.Action;
+import com.jfcorugedo.creational.factorymethod.actions.WritableAction;
 import com.jfcorugedo.creational.factorymethod.behaviours.Clickable;
 
 public class InputText implements Clickable {
     @Override
     public Action click() {
-        return null;
+        return new WritableAction();
     }
 }
