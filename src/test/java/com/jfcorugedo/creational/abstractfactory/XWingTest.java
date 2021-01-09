4555572755950268061a9502cb0639de51ab4e6f
jfcorugedo <jfcorugedo@gmail.com>
2021-01-09 19:20:46 +0100

    feat(AbstractFactory): adds XWingTest.initialShield. Lets start with the simplest one

---
+++ src/test/java/com/jfcorugedo/creational/abstractfactory/XWingTest.java
@@ -0,0 +1,24 @@
+package com.jfcorugedo.creational.abstractfactory;
+
+import com.jfcorugedo.creational.abstractfactory.starfighter.XWing;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * TDD tests list:
+ * - X-Wing Starfighter moves 1458 meters per round (one round is 10 seconds)
+ * - X-Wing Starfighter attack produces 4 laser impacts that hits everything in front of it at 1000 meters or less
+ * - X-Wing Starfighter has 50 units of shield to protect itself
+ */
+public class XWingTest {
+
+    /* Lets start with the simplest one following the rule Start Simple */
+    @Test
+    public void initialShield() {
+
+        XWing xWing = new XWing();
+
+        assertThat(xWing.getShield()).isEqualTo(50);
+    }
+}
