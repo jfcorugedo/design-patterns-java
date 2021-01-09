787048e6c3df89281de186a7dccab6c7b0ac759e
jfcorugedo <jfcorugedo@gmail.com>
2021-01-09 19:39:07 +0100

    feat(AbstractFactory): refactor (eliminate duplicates). It creates a currentPosition inside XWing to track its position at any time

--- src/main/java/com/jfcorugedo/creational/abstractfactory/starfighter/XWing.java
+++ src/main/java/com/jfcorugedo/creational/abstractfactory/starfighter/XWing.java
@@ -6,9 +6,11 @@ import java.awt.geom.Point2D;
 public class XWing {
 
     private int shield;
+    private Point2D currentPosition;
 
-    public XWing(int shield) {
+    public XWing(int shield, int x, int y) {
         this.shield = shield;
+        this.currentPosition = new Point(x, y);
     }
 
     public int getShield() {
