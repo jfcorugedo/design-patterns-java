ff859472a5b951b5454a21674eb5b103556a87fe
jfcorugedo <jfcorugedo@gmail.com>
2021-01-10 19:05:02 +0100

    feat(AbstractFactory): makes NeutronCruiserTest.move pass using obvious implementation

--- src/main/java/com/jfcorugedo/creational/abstractfactory/cruiser/NeutronCruiser.java
+++ src/main/java/com/jfcorugedo/creational/abstractfactory/cruiser/NeutronCruiser.java
@@ -1,13 +1,24 @@
 package com.jfcorugedo.creational.abstractfactory.cruiser;
 
+import java.awt.geom.Point2D;
+
 public class NeutronCruiser extends Cruiser {
+    private int speedPerRound = 1400;
+
     public NeutronCruiser(int shield, int x, int y, double initialAngle) {
         super(shield, x, y, initialAngle);
     }
 
+    public int getSpeedPerRound() {
+        return this.speedPerRound;
+    }
+
     @Override
     public void move() {
+        double newX = this.getCurrentPosition().getX() + getSpeedPerRound() *Math.cos(this.getDirectionAngle());
+        double newY = this.getCurrentPosition().getY() + getSpeedPerRound()*Math.sin(this.getDirectionAngle());
 
+        this.setCurrentPosition(new Point2D.Double(newX, newY));
     }
 
     @Override
