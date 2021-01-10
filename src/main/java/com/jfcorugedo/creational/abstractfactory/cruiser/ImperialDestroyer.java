81a8b5294127aa898fe389519868cae94ed2e14b
jfcorugedo <jfcorugedo@gmail.com>
2021-01-10 17:52:45 +0100

    feat(AbstractFactory): makes ImperialDestroyerTest.move compile and pass using Fake it

--- src/main/java/com/jfcorugedo/creational/abstractfactory/cruiser/ImperialDestroyer.java
+++ src/main/java/com/jfcorugedo/creational/abstractfactory/cruiser/ImperialDestroyer.java
@@ -1,5 +1,8 @@
 package com.jfcorugedo.creational.abstractfactory.cruiser;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+
 public class ImperialDestroyer {
     private int shield;
 
@@ -9,5 +12,12 @@ public class ImperialDestroyer {
 
     public int getShield() {
         return this.shield;
+    }
+
+    public void move() {
+    }
+
+    public Point2D getPosition() {
+        return new Point2D.Double(1354, 0);
     }
 }
