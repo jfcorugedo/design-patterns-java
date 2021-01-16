758a476a882203fae7af7786c85d1c41262508f8
jfcorugedo <jfcorugedo@gmail.com>
2021-01-16 20:09:11 +0100

    feat(Adapter): make it pass using obvious implementation

--- src/main/java/com/jfcorugedo/structural/adapter/Employee.java
+++ src/main/java/com/jfcorugedo/structural/adapter/Employee.java
@@ -2,12 +2,19 @@ package com.jfcorugedo.structural.adapter;
 
 import lombok.Builder;
 
+import java.util.Set;
+
 @Builder
 public class Employee {
 
     private String firstName;
+    private Set<String> skills;
 
     public String getFullName() {
         return this.firstName;
+    }
+
+    public Set<String> getSkills() {
+        return this.skills;
     }
 }
