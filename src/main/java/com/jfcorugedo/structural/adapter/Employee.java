bff29a00f3fb9a4ea5968605d1568f6a7862b5fb
jfcorugedo <jfcorugedo@gmail.com>
2021-01-16 19:38:41 +0100

    feat(Adapter): make it pass using Fake It approach

---
+++ src/main/java/com/jfcorugedo/structural/adapter/Employee.java
@@ -0,0 +1,13 @@
+package com.jfcorugedo.structural.adapter;
+
+import lombok.Builder;
+
+@Builder
+public class Employee {
+
+    private String firstName;
+
+    public String getFullName() {
+        return "Juan Corugedo";
+    }
+}
